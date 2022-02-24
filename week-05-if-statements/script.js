cc05c026e806321e4e3efe9b40defd6b986ba73a
EmJonesyy <98540612+EmJonesyy@users.noreply.github.com>
2022-02-24 14:25:34 -0500

    changes

--- week-05-if-statements/script.js
+++ week-05-if-statements/script.js
@@ -25,11 +25,84 @@ function submitAnswerTwo() {
   // Get the number from answer two's input element
   const answerTwo = answerTwoInput.value;
 
-  if(answerTwo == 42) {
+  if(answerTwo == 22) {
     alert("That's right!");
     hide('question-two');
     show('done');
-  } else if(answerTwo < 42) {
+  } else if(answerTwo < 22) {
+    alert('The answer is higher.');
+  } else {
+    alert('The answer is lower.')
+  }
+}
+// When this function is called, it takes the user's answer to the third
+// question and uses if statements to check whether it's correct.
+function submitAnswerThree() {
+  // Get answer threes's input element
+  const answerThreeInput = document.getElementById('answer-three-input');
+  // Get the text from answer three's input element
+  const answerThree = answerThreeInput.value;
+
+  if(answerThree == 'Ray') {
+    alert("That's right!");
+    hide('question-three');
+    show('question-four');
+  } else if(answerThree == 'john') {
+    alert('my boyfriend's name starts with an R.');
+  }
+}
+
+// When this function is called, it takes the user's answer to the fourth
+// question and uses if statements to check whether it's correct.
+function submitAnswerFour() {
+  // Get answer four's input element
+  const answerFourInput = document.getElementById('answer-four-input');
+  // Get the number from answer four's input element
+  const answerFour = answerFourInput.value;
+
+  if(answerFour == 1999) {
+    alert("That's right!");
+    hide('question-four');
+    show('done');
+  } else if(answerfour < 1999) {
+    alert('The answer is higher.');
+  } else {
+    alert('The answer is lower.')
+  }
+}
+
+// When this function is called, it takes the user's answer to the fifth
+// question and uses if statements to check whether it's correct.
+function submitAnswerFive() {
+  // Get answer five's input element
+  const answerFiveInput = document.getElementById('answer-five-input');
+  // Get the text from answer fives's input element
+  const answerFive = answerFiveInput.value;
+
+  if(answerFive == 'Buddy') {
+    alert("That's right!");
+    hide('question-five');
+    show('question-six');
+  } else if(answerFive == 'Bud') {
+    alert('Close, but it's a longer name.');
+  } else {
+    alert('Wrong! Hint: Ends with a y.')
+  }
+}
+
+// When this function is called, it takes the user's answer to the sixth
+// question and uses if statements to check whether it's correct.
+function submitAnswerSix() {
+  // Get answer six's input element
+  const answerSixInput = document.getElementById('answer-six-input');
+  // Get the number from answer sixth's input element
+  const answerSix = answerSixInput.value;
+
+  if(answerSix == 6) {
+    alert("That's right!");
+    hide('question-six');
+    show('done');
+  } else if(answerSix < 6) {
     alert('The answer is higher.');
   } else {
     alert('The answer is lower.')
