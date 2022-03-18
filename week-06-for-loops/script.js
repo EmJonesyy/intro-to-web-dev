d823ecfe9628489d09f6076d0c50ca91a4f1b254
EmJonesyy <98540612+EmJonesyy@users.noreply.github.com>
2022-03-18 15:40:40 -0400

    short words

--- week-06-for-loops/script.js
+++ week-06-for-loops/script.js
@@ -1,12 +1,11 @@
 // Replace this speeh with your own.
 // Notice the `` backticks, which let you split your string into multiple lines.
-const speech = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-    est laborum.`;
+const speech = `General Kenobi. Years ago you served my father in the Clone Wars.
+Now he begs you to help him in his struggle against the Empire. I regret that I am unable to present my fathers request to you in person
+but my ship has fallen under attack and I'm afraid my mission to bring you to Alderaan has failed. 
+I have placed information vital to the survival of the Rebellion into the memory systems of this R2 unit.
+My father will know how to retrieve it. You must see this droid safely delivered to him on Alderaan. 
+This is our most desperate hour. Help me Obi-Wan Kenobi. Youre my only hope.`;
 
 // Remove punctuation from the speech. You might have to modify this if your
 // speech contains other punctuation.
@@ -31,6 +30,21 @@ function displayLongWords() {
       longWordsElement.appendChild(wordElement);
     }
   }
+ // Displays words that have less than 4 characters.
+function displayshortWords() {
+  const shortWordsElement = document.getElementById('short-words');
+
+  // Loop over every word in the array.
+  for(let i = 0; i < wordsArray.length; i++) {
+    const word = wordsArray[i];
+    // If the word has less than 4 characters, display it in the page.
+    if(word.length 4 < ) {
+      const wordElement = document.createElement('li');
+      wordElement.innerText = word;
+      shortWordsElement.appendChild(wordElement); 
+    }
+  }
+    
 }
 
 // TODO: Define your own functions here!
