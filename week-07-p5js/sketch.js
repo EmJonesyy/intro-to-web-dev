f9da97bbb61c3968203ea46adf788ed1d26fc8a8
EmJonesyy <98540612+EmJonesyy@users.noreply.github.com>
2022-03-18 17:38:23 -0400

    changes

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -1,1 +1,17 @@
+let message = 'I LOVE THE COLOR PINK' ;
+let textY = 300;
+letySpeed = 3;
 
+function setup()  {
+createCanvas (500, 600);
+}
+
+function draw() {
+ background (225);
+  
+  textSize (25);
+  text(message, 160, textY);
+  
+  textY  +=  ySpeed;
+}
+  
